e0ad14f08bd9040e2a661f1c130168782137b034
Subash <suubashsss22@gmail.com>
2021-03-02 21:31:42 +0530

    First commit

---
+++ src/product/Product.java
@@ -0,0 +1,19 @@
+package product;
+
+public class Product {
+	public String skuId;
+	public String price;
+	
+	public String getSkuId() {
+		return skuId;
+	}
+	public void setSkuId(String skuId) {
+		this.skuId = skuId;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
