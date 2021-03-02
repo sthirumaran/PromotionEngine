f1d1c3b6bcf48e9cef18130a1acbfedd2af94462
Subash <suubashsss22@gmail.com>
2021-03-02 22:40:49 +0530

    Commit

--- src/product/Product.java
+++ src/product/Product.java
@@ -1,19 +1,52 @@
 package product;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import productservice.ProductService;
+
 public class Product {
+	public Product(String skuId) {
+		super();
+		this.skuId = skuId;
+		ProductService productService=new ProductService();
+		productService.getPriceByType(this);
+	}
 	public String skuId;
-	public String price;
-	
+	public int price;
+
 	public String getSkuId() {
 		return skuId;
 	}
 	public void setSkuId(String skuId) {
 		this.skuId = skuId;
 	}
-	public String getPrice() {
+
+	public int getPrice() {
 		return price;
 	}
-	public void setPrice(String price) {
+	public void setPrice(int price) {
 		this.price = price;
+	}
+	
+
+	public static void main(String args[]) {
+		List<Product> products = new ArrayList<Product>();
+		Scanner in = new Scanner(System.in); 
+		System.out.println("total number of order");
+		String s = in.nextLine(); 
+
+		int a = Integer.parseInt(s);
+		for (int i = 0; i < a; i++)
+		{                
+			System.out.println("enter the type of product:A,B,C or D");
+			String type= in.nextLine(); 
+			Product p = new Product(type);
+			products.add(p);
+		}
+		ProductService productService=new ProductService();
+		int totalPrice = productService.getTotalPrice(products);
+		System.out.println(totalPrice);
 	}
 }
